4a4da417596f5b6d697746120d69f403f26fd304
Nicolas Ferreira <nickrfer@gmail.com>
2017-04-23 20:43:59 -0300

    Fixing codacy bugs.

--- test/nodejs-svn-git-diff-test.js
+++ test/nodejs-svn-git-diff-test.js
@@ -1,9 +1,9 @@
-var assert = require('assert');
-var diff = require('../src/nodejs-svn-git-diff.js');
+var assert = require("assert");
+var diff = require("../src/nodejs-svn-git-diff.js");
 
-describe('nodejs-svn-git-diff', function() {
-  describe('parse', function() {
-    it('should parse svn revision diff to git diff', function() {
+describe("nodejs-svn-git-diff", function() {
+  describe("parse", function() {
+    it("should parse svn revision diff to git diff", function() {
         // TODO: do some actual testing.
         assert.equal(1, 1);
     });
